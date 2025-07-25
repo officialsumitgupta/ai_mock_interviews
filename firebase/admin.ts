d60d54a6763096175ee5416cd11a3b0118a91968
Sumit Gupta <thesumitguptaofficial@gmail.com>
2025-07-25 00:45:42 -0700

    Fix Some Error

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,24 +1,26 @@
-import {getApps} from 'firebase-admin/app';
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "firebase-admin/firestore";
+import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 
-const initFirebaseAdmin = () => {
-    const app = getApp();
+// Initialize Firebase Admin SDK
+function initFirebaseAdmin() {
+    const apps = getApps();
 
-    if (!app.length) {
-         initializeApp({
-             credential: cert({
-                 projectId: process.env.FIREBASE_PROJECT_ID,
-                 clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                 privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
-             })
-         })
+    if (!apps.length) {
+        initializeApp({
+            credential: cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                // Replace newlines in the private key
+                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+            }),
+        });
     }
 
     return {
         auth: getAuth(),
-        db: getFirestore()
-    }
+        db: getFirestore(),
+    };
 }
 
-export const {auth, db} = initFirebaseAdmin();
+export const { auth, db } = initFirebaseAdmin();
